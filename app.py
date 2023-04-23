9aae25003ecaf4169063d13ceb30df663d43a37c
Joseph <josephomoregie1@outlook.com>
2023-04-23 03:43:33 +0100

    changed file structure and made changes to index.html

--- app.py
+++ app.py
@@ -1,41 +1,9 @@
-from flask import render_template, request
-from WebApp.app.sequence_analysis import read_tsv, find_differences, determine_change
 from WebApp.app import create_app
+from WebApp.app.core import core
 import os
-import tempfile
 
 app = create_app()
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    """
-    Index route to handle the file upload and processing of TSV files containing DNA sequences.
-    """
-    if request.method == "POST":
-        tsv_file = request.files["tsv_file"]
-
-        if tsv_file:
-            # Save the uploaded file to a temporary file
-            temp_file = tempfile.NamedTemporaryFile(delete=False)
-            tsv_file.save(temp_file.name)
-            temp_file.close()  # Close the file after saving
-
-            # Read the data from the TSV file
-            data = read_tsv(temp_file.name)
-
-            # Remove the temporary file
-            os.unlink(temp_file.name)
-
-            # Process the data
-            counts = find_differences(data)
-            change_messages = [determine_change(sequence_data) for sequence_data in data]
-
-            # Render the template with the processed data
-            return render_template("index.html", counts=counts, data=data, data_and_changes=zip(data, change_messages))
-
-    return render_template("index.html")
-
+app.register_blueprint(core)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
